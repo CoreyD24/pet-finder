6dafbe358ea2e8d4c0828097ba1e685e8bc8614b
coreyfsa <coreyfsa@DESKTOP-TCPPHAT.localdomain>
2023-11-28 08:09:05 -0700

    completed

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require(`express`);
 const data = require("./data");
 const app = express();
 
-const state = {
-    allNames:[]
-}
 
 const PORT = 5173;
 app.listen(PORT, () => {
     console.log(`listening on port number ${PORT}`)
+})
+
+app.get("/api/v1/pets/owner", (req, res) => {
+    const {owner} = req.query;
+    const filtered = data.filter(
+        (pet) => pet.owner.toLowerCase() === owner.toLowerCase ()
+    )
+    res.send(filtered);
 })
 
 //reference only in case I need it
@@ -29,6 +34,10 @@ app.get(`/api/v1/pets`,(req, res) => {
 
 //this is to get a specific pet name
 app.get(`/api/v1/pets/:name`, (req, res) => {
-    res.send(`<h2>Pet Name is: ${req.params.name}`)
+    const {name} = req.params
+    const filtered = data.filter(
+        (pet) => pet.name.toLowerCase() === name.toLowerCase()
+    );
+   res.send(filtered)
 })
 
